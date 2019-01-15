3cd4a27d5cf25445436b74ef853c009b971fe5e1
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-01-15 15:36:39 +0000

    Added DialogPane

--- todolist/Controller.java
+++ todolist/Controller.java
@@ -3,13 +3,14 @@ package todolist;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import todolist.datamodel.TodoData;
 import todolist.datamodel.TodoItem;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -25,27 +26,10 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadlineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     public void initialize(){
-//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy birthday card for Clare",
-//                LocalDate.of(2019, Month.JANUARY, 7));
-//        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Blake. Bring paperwork",
-//                LocalDate.of(2019, Month.JANUARY, 18));
-//        TodoItem item3 = new TodoItem("Finish data science project", "I promised Vinny draft by 29th January ",
-//                LocalDate.of(2019, Month.JANUARY, 29));
-//        TodoItem item4 = new TodoItem("Pickup Jim from airport", "Jim lands 23rd December at 4.15pm",
-//                LocalDate.of(2018, Month.DECEMBER, 23));
-//        TodoItem item5 = new TodoItem("Pickup dry cleaning", "Clothes should be ready by Wednesday",
-//                LocalDate.of(2019, Month.JANUARY, 2));
-
-//        todoItems = new ArrayList<>();
-//        todoItems.add(item1);
-//        todoItems.add(item2);
-//        todoItems.add(item3);
-//        todoItems.add(item4);
-//        todoItems.add(item5);
-//
-//        TodoData.getInstance().setTodoItems(todoItems);
 
         // automatically select first item in list and display its details
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
@@ -63,7 +47,20 @@ public class Controller {
         todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
+    }
 
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        }catch(IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
     }
 
     @FXML
@@ -71,11 +68,7 @@ public class Controller {
         TodoItem item = todoListView.getSelectionModel().getSelectedItem();
         itemDetailsTextArea.setText(item.getDetails());
         deadlineLabel.setText(item.getDeadline().toString());
-//        StringBuilder sb = new StringBuilder(item.getDetails());
-//        sb.append("\n\n\n\n");
-//        sb.append("Due ");
-//        sb.append(item.getDeadline().toString());
-//        itemDetailsTextArea.setText(sb.toString());
+
     }
 
 }
---
+++ todolist/DialogController.java
@@ -0,0 +1,11 @@
+package todolist;
+
+/**
+ * Created by Johnny on 15/01/2019
+ * COMMENTS ABOUT THE PROGRAM GO HERE
+ */
+public class DialogController {
+    public static void main(String[] args) {
+
+    }
+}
