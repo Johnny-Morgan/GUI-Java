ece0e31a36577246a81b19a1281e5069e089c5e1
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-01-16 13:56:23 +0000

    Updated list view

--- todolist/DialogController.java
+++ todolist/DialogController.java
@@ -1,11 +1,35 @@
 package todolist;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import todolist.datamodel.TodoData;
+import todolist.datamodel.TodoItem;
+
+
+import java.time.LocalDate;
+
 /**
  * Created by Johnny on 15/01/2019
  * COMMENTS ABOUT THE PROGRAM GO HERE
  */
 public class DialogController {
-    public static void main(String[] args) {
 
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults(){
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
     }
 }
--- todolist/datamodel/TodoData.java
+++ todolist/datamodel/TodoData.java
@@ -36,9 +36,10 @@ public class TodoData {
         return todoItems;
     }
 
-//    public void setTodoItems(List<TodoItem> todoItems) {
-//        this.todoItems = todoItems;
-//    }
+    public void addTodoItem(TodoItem item){
+        todoItems.add(item);
+    }
+
 
     public void loadTodoItems()throws IOException{
         todoItems = FXCollections.observableArrayList();
@@ -82,4 +83,5 @@ public class TodoData {
             }
         }
     }
+
 }
